28d767687c67baf57adfe9d9720fd126e652b2be
jackyliu16 <18922251299@163.com>
2024-11-16 20:10:57 +0800

    remove useless part like counters

--- src/ui.rs
+++ src/ui.rs
@@ -4,7 +4,7 @@ use ratatui::{
     widgets::{Block, BorderType, Paragraph},
     Frame,
 };
-
+use ratatui::text::Text;
 use crate::app::App;
 
 /// Renders the user interface widgets.
@@ -14,13 +14,7 @@ pub fn render(app: &mut App, frame: &mut Frame) {
     // - https://docs.rs/ratatui/latest/ratatui/widgets/index.html
     // - https://github.com/ratatui/ratatui/tree/master/examples
     frame.render_widget(
-        Paragraph::new(format!(
-            "This is a tui template.\n\
-                Press `Esc`, `Ctrl-C` or `q` to stop running.\n\
-                Press left and right to increment and decrement the counter respectively.\n\
-                Counter: {}",
-            app.counter
-        ))
+        Paragraph::new(Text::from("Hello, World !"))
         .block(
             Block::bordered()
                 .title("Template")
